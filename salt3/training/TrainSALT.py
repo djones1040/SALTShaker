eb021aa4bb52203706f7d1aab8b4a47f04ae5834
D'Arcy Kenworthy <wkenwor1@jhu.edu>
2019-03-21 13:55:45 -0400

    Moved code to read in photometry and spectra to separate file from TrainSALT

--- salt3/training/TrainSALT.py
+++ salt3/training/TrainSALT.py
@@ -10,7 +10,7 @@ import sys
 import multiprocessing
 from scipy.linalg import lstsq
 
-from salt3.util import snana
+from salt3.util import snana,readutils
 from salt3.util.estimate_tpk_bazin import estimate_tpk_bazin
 from scipy.optimize import minimize, least_squares, differential_evolution
 from salt3.training.fitting import fitting
@@ -168,224 +168,6 @@ class TrainSALT:
 		
 		return parser
 
-	def rdkcor(self,kcorpath):
-
-		self.kcordict = {}
-		for k in kcorpath:
-			survey,kcorfile = k.split(',')
-			kcorfile = os.path.expandvars(kcorfile)
-			if not os.path.exists(kcorfile):
-				raise RuntimeError('kcor file %s does not exist'%kcorfile)
-			self.kcordict[survey] = {}
-
-			try:
-				hdu = fits.open(kcorfile)
-				zpoff = hdu[1].data
-				snsed = hdu[2].data
-				filtertrans = hdu[5].data
-				primarysed = hdu[6].data
-				hdu.close()
-			except:
-				raise RuntimeError('kcor file format is non-standard')
-
-			self.kcordict[survey]['filtwave'] = filtertrans['wavelength (A)']
-			self.kcordict[survey]['primarywave'] = primarysed['wavelength (A)']
-			self.kcordict[survey]['snflux'] = snsed['SN Flux (erg/s/cm^2/A)']
-			if 'AB' in primarysed.names:
-				self.kcordict[survey]['AB'] = primarysed['AB']
-			if 'Vega' in primarysed.names:
-				self.kcordict[survey]['Vega'] = primarysed['Vega']
-			if 'BD17' in primarysed.names:
-				self.kcordict[survey]['BD17'] = primarysed['BD17']
-			for filt in zpoff['Filter Name']:
-				self.kcordict[survey][filt.split('-')[-1].split('/')[-1]] = {}
-				self.kcordict[survey][filt.split('-')[-1].split('/')[-1]]['filttrans'] = filtertrans[filt]
-				self.kcordict[survey][filt.split('-')[-1].split('/')[-1]]['zpoff'] = \
-					zpoff['ZPOff(Primary)'][zpoff['Filter Name'] == filt][0]
-				self.kcordict[survey][filt.split('-')[-1].split('/')[-1]]['magsys'] = \
-					zpoff['Primary Name'][zpoff['Filter Name'] == filt][0]
-				self.kcordict[survey][filt.split('-')[-1].split('/')[-1]]['primarymag'] = \
-					zpoff['Primary Mag'][zpoff['Filter Name'] == filt][0]
-
-		initBfilt = '%s/Bessell90_B.dat'%init_rootdir
-		filtwave,filttp = np.genfromtxt(initBfilt,unpack=True)
-			
-		self.kcordict['default'] = {}
-		self.kcordict['default']['Bwave'] = filtwave
-		self.kcordict['default']['Btp'] = filttp
-				
-	def rdSpecData(self,datadict,speclist):
-		if not os.path.exists(speclist):
-			raise RuntimeError('speclist %s does not exist')
-		
-		try:
-			snid,mjd,specfiles = np.genfromtxt(speclist,unpack=True,dtype='str')
-			snid,mjd,specfiles = np.atleast_1d(snid),np.atleast_1d(mjd),np.atleast_1d(specfiles)
-			snanaSpec=False
-		except:
-			specfiles=np.genfromtxt(speclist,dtype='str')
-			specfiles=np.atleast_1d(specfiles)
-			snanaSpec=True
-			
-		if snanaSpec:
-			for sf in specfiles:
-			
-				if '/' not in sf:
-					sf = '%s/%s'%(os.path.dirname(speclist),sf)
-				if not os.path.exists(sf):
-					raise RuntimeError('specfile %s does not exist'%sf)
-				sn=snana.SuperNova(sf)
-				s=sn.name
-				if s in datadict.keys():
-					tpk=datadict[s]['tpk']
-					if 'specdata' not in datadict[s].keys():
-						datadict[s]['specdata'] = {}
-						speccount = 0
-					else:
-						speccount = len(datadict[s]['specdata'].keys())
-						
-					if len(sn.SPECTRA)==0:
-						raise ValueError('File {} contains no supernova spectra'.format(sf))
-					for k in sn.SPECTRA:
-						spec=sn.SPECTRA[k]
-						m=spec['SPECTRUM_MJD']
-						datadict[s]['specdata'][speccount] = {}
-						datadict[s]['specdata'][speccount]['fluxerr'] = spec['FLAMERR']
-						datadict[s]['specdata'][speccount]['wavelength'] = (spec['LAMMIN']+spec['LAMMAX'])/2
-						datadict[s]['specdata'][speccount]['flux'] = spec['FLAM']
-						datadict[s]['specdata'][speccount]['tobs'] = m - tpk
-						datadict[s]['specdata'][speccount]['mjd'] = m
-						speccount+=1
-				else:
-					print('SNID %s has no photometry so I\'m ignoring it')
-
-		else:
-			for s,m,sf in zip(snid,mjd,specfiles):
-				try: m = float(m)
-				except: m = snana.date_to_mjd(m)
-
-				if '/' not in sf:
-					sf = '%s/%s'%(os.path.dirname(speclist),sf)
-					
-				if not os.path.exists(sf):
-					raise RuntimeError('specfile %s does not exist'%sf)
-			
-				if s in datadict.keys():
-					tpk=datadict[s]['tpk']
-					if 'specdata' not in datadict[s].keys():
-						datadict[s]['specdata'] = {}
-						speccount = 0
-					else:
-						speccount = len(datadict[s]['specdata'].keys())
-			
-					try:
-						wave,flux,fluxerr = np.genfromtxt(sf,unpack=True,usecols=[0,1,2])
-			
-					except:
-						wave,flux = np.genfromtxt(sf,unpack=True,usecols=[0,1])
-						fluxerr=np.tile(np.nan,flux.size)
-
-					datadict[s]['specdata'][speccount] = {}
-					datadict[s]['specdata'][speccount]['fluxerr'] = fluxerr
-					datadict[s]['specdata'][speccount]['wavelength'] = wave
-					datadict[s]['specdata'][speccount]['flux'] = flux
-					datadict[s]['specdata'][speccount]['tobs'] = m - tpk
-					datadict[s]['specdata'][speccount]['mjd'] = m
-				else:
-					print('SNID %s has no photometry so I\'m ignoring it')
-
-		return datadict
-
-	def rdAllData(self,snlist,speclist=None):
-		datadict = {}
-
-		if not os.path.exists(snlist):
-			raise RuntimeError('SN list %s doesn\'t exist'%snlist)
-		snfiles = np.genfromtxt(snlist,dtype='str')
-		snfiles = np.atleast_1d(snfiles)
-
-		for f in snfiles:
-			if f.lower().endswith('.fits'):
-				raise RuntimeError('FITS extensions are not supported yet')
-
-			if '/' not in f:
-				f = '%s/%s'%(os.path.dirname(snlist),f)
-			sn = snana.SuperNova(f)
-
-			if sn.SNID in datadict.keys():
-				self.addwarning('SNID %s is a duplicate!  Skipping'%sn.SNID)
-				continue
-
-			if not 'SURVEY' in sn.__dict__.keys():
-				raise RuntimeError('File %s has no SURVEY key, which is needed to find the filter transmission curves'%PhotSNID[0])
-			if not 'REDSHIFT_HELIO' in sn.__dict__.keys():
-				raise RuntimeError('File %s has no heliocentric redshift information in the header'%PhotSNID[0])
-
-			if 'PEAKMJD' in sn.__dict__.keys(): sn.SEARCH_PEAKMJD = sn.PEAKMJD
-			zHel = float(sn.REDSHIFT_HELIO.split('+-')[0])
-			if self.options.estimate_tpk:
-				if 'B' in sn.FLT:
-					tpk,tpkmsg = estimate_tpk_bazin(
-						sn.MJD[sn.FLT == 'B'],sn.FLUXCAL[sn.FLT == 'B'],sn.FLUXCALERR[sn.FLT == 'B'],max_nfev=100000,t0=sn.SEARCH_PEAKMJD)
-				elif 'g' in sn.FLT:
-					tpk,tpkmsg = estimate_tpk_bazin(
-						sn.MJD[sn.FLT == 'g'],sn.FLUXCAL[sn.FLT == 'g'],sn.FLUXCALERR[sn.FLT == 'g'],max_nfev=100000,t0=sn.SEARCH_PEAKMJD)
-				elif 'c' in sn.FLT:
-					tpk,tpkmsg = estimate_tpk_bazin(
-						sn.MJD[sn.FLT == 'c'],sn.FLUXCAL[sn.FLT == 'c'],sn.FLUXCALERR[sn.FLT == 'c'],max_nfev=100000,t0=sn.SEARCH_PEAKMJD)
-				else:
-					raise RuntimeError('need a blue filter to estimate tmax')
-			else:
-				tpk = sn.SEARCH_PEAKMJD
-				tpkmsg = 'termination condition is satisfied'
-
-			# at least one epoch 3 days before max
-			if not len(sn.MJD[sn.MJD < tpk-3]):
-				self.addwarning('skipping SN %s; no epochs 3 days pre-max'%sn.SNID)
-				continue
-
-			if 'termination condition is satisfied' not in tpkmsg:
-				self.addwarning('skipping SN %s; can\'t estimate t_max'%sn.SNID)
-				continue
-
-			datadict[sn.SNID] = {'snfile':f,
-								 'zHelio':zHel,
-								 'survey':sn.SURVEY,
-								 'tpk':tpk}
-			#datadict[snid]['zHelio'] = zHel
-			
-			# TODO: flux errors
-			datadict[sn.SNID]['specdata'] = {}
-# 			for k in sn.SPECTRA.keys():
-# 				datadict[sn.SNID]['specdata'][k] = {}
-# 				datadict[sn.SNID]['specdata'][k]['specphase'] = sn.SPECTRA[k]['SPECTRUM_MJD']
-# 				datadict[sn.SNID]['specdata'][k]['tobs'] = sn.SPECTRA[k]['SPECTRUM_MJD'] - tpk
-# 				datadict[sn.SNID]['specdata'][k]['mjd'] = sn.SPECTRA[k]['SPECTRUM_MJD']
-# 				if 'LAMAVG' in sn.SPECTRA[k].keys():
-# 					datadict[sn.SNID]['specdata'][k]['wavelength'] = sn.SPECTRA[k]['LAMAVG']
-# 				elif 'LAMMIN' in sn.SPECTRA[k].keys() and 'LAMMAX' in sn.SPECTRA[k].keys():
-# 					datadict[sn.SNID]['specdata'][k]['wavelength'] = np.mean([[sn.SPECTRA[k]['LAMMIN']],
-# 																			  [sn.SPECTRA[k]['LAMMAX']]],axis=0)
-# 				else:
-# 					raise RuntimeError('couldn\t find wavelength data in photometry file')
-# 				datadict[sn.SNID]['specdata'][k]['flux'] = sn.SPECTRA[k]['FLAM']
-# 				datadict[sn.SNID]['specdata'][k]['fluxerr'] = sn.SPECTRA[k]['FLAMERR']
-# 				
-			datadict[sn.SNID]['photdata'] = {}
-			datadict[sn.SNID]['photdata']['tobs'] = sn.MJD - tpk
-			datadict[sn.SNID]['photdata']['mjd'] = sn.MJD
-			datadict[sn.SNID]['photdata']['fluxcal'] = sn.FLUXCAL
-			datadict[sn.SNID]['photdata']['fluxcalerr'] = sn.FLUXCALERR
-			datadict[sn.SNID]['photdata']['filt'] = sn.FLT
-
-		if not len(datadict.keys()):
-			raise RuntimeError('no light curve data to train on!!')
-			
-		if speclist:
-			datadict = self.rdSpecData(datadict,speclist)
-			
-		return datadict
-		
 	def fitSALTModel(self,datadict,phaserange,phaseres,waverange,waveres,
 					 colorwaverange,fitmethod,fitstrategy,fititer,kcordict,initmodelfile,initBfilt,
 					 phaseoutres,waveoutres,regulargradientphase, regulargradientwave, regulardyad,n_min_specrecal,specrange_wavescale_specrecal,n_specrecal_per_lightcurve,filter_mass_tolerance ,n_components=1,n_colorpars=0,n_processes=1):
@@ -709,11 +491,11 @@ Salt2ExtinctionLaw.max_lambda %i"""%(
 
 		if not self.options.kcor_path:
 			raise RuntimeError('kcor_path variable must be defined!')
-		self.rdkcor(self.options.kcor_path)
+		self.kcordict=readutils.rdkcor(self.options.kcor_path,self.addwarning)
 		# TODO: ASCII filter files
 		
 		# read the data
-		datadict = self.rdAllData(self.options.snlist,speclist=self.options.speclist)
+		datadict = readutils.rdAllData(self.options.snlist,self.options.estimate_tpk,self.addwarning,speclist=self.options.speclist)
 		
 		if not os.path.exists(self.options.outputdir):
 			os.makedirs(self.options.outputdir)
