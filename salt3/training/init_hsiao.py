restructured code, fixed normalization issues

salt3/training/init_hsiao.py
@@ -2,6 +2,7 @@
 import numpy as np
 from scipy.interpolate import bisplrep,bisplev
 from scipy.interpolate import interp1d
+from sncosmo.constants import HC_ERG_AA
 
 _SCALE_FACTOR = 1e-12
 
@@ -15,14 +16,8 @@ def init_hsiao(hsiaofile='initfiles/Hsiao07.dat',
 
 	phase,wave,flux = np.loadtxt(hsiaofile,unpack=True)
 	refWave,refFlux=np.loadtxt(flatnu,unpack=True)
-	#sphase,swave,sflux = np.loadtxt(hsiaofile,unpack=True)
-	#HBmag = synphotB(wave[phase == 0],flux[phase == 0],0,Bfilt=Bfilt)
-	#SBmag = synphotB(swave[phase == 0],sflux[sphase == 0],0,Bfilt=Bfilt)
-	#import pdb; pdb.set_trace()
-	
-	#flux /= 4*np.pi*(10*3.086e18)**2.
-	#flux *= 10**(0.4*19.36)*_SCALE_FACTOR
-	flux *= 10**(-0.4*(10.635+(synphotB(refWave,refFlux,0,0,Bfilt)-synphotB(wave[phase==0],flux[phase==0],0,0,Bfilt))))*_SCALE_FACTOR
+
+	flux *= 10**(-0.4*(-19.36+(synphotB(refWave,refFlux,0,0,Bfilt)-synphotB(wave[phase==0],flux[phase==0],0,0,Bfilt))))#*_SCALE_FACTOR
 	
 	m1phase = phase*1.1
 	splinephase = np.linspace(phaserange[0],phaserange[1],
@@ -49,19 +44,11 @@ def init_hsiao(hsiaofile='initfiles/Hsiao07.dat',
 		import pylab as plt
 		plt.ion()
 		plt.plot(wave[phase == 0],flux[phase == 0],label='hsiao, phase = 0 days')
-		#plt.plot(wave[phase == 1],flux[phase == 1],label='hsiao, phase = +1 day')
-		#plt.plot(intwave,m0[7,:],label='interp')
 		m0test = bisplev(np.array([0]),intwave,(bspl[0],bspl[1],bspl[2],3,3))
 		plt.xlim([2000,9200])
 		plt.legend()
 		plt.plot(intwave,m0test,label='interp')
-
-		#bspl[2][bspl[2] < 0] = 0
 		bspltmp = bspl[2].reshape([len(splinephase)-4,len(splinewave)-4])
-		#bspltmp[bspltmp
-		#m0test = bisplev(np.array([0]),intwave,(bspl[0],bspl[1],bspltmp.flatten(),3,3))
-		#plt.plot(intwave,m0test,label='interp')
-		import pdb; pdb.set_trace()
 	
 	return intphase,intwave,m0,m1,bspl[0],bspl[1],bspl[2],bsplm1[2]
 
@@ -118,5 +105,5 @@ def synphotB(sourcewave,sourceflux,zpoff,redshift=0,
 	pbspl = np.interp(obswave[g],filtwave,filttrans)
 	pbspl *= obswave[g]
 
-	res = np.trapz(pbspl*sourceflux[g],obswave[g])/np.trapz(pbspl,obswave[g])
+	res = np.trapz(pbspl*sourceflux[g]/HC_ERG_AA,obswave[g])/np.trapz(pbspl,obswave[g])
 	return(zpoff-2.5*np.log10(res))