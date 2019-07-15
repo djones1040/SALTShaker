Test for spectral recalibration jacobian Spectral recalibration jacobian incorrect in previous commit, now fixed Spectral recalibration invoked in robust_process_fit and lsqwrap Also trying to fix validatelightcurves

salt3/training/saltresids.py
@@ -81,6 +81,7 @@ class SALTResids:
 		self.im0 = np.where(self.parlist == 'm0')[0]
 		self.im1 = np.where(self.parlist == 'm1')[0]
 		self.iCL = np.where(self.parlist == 'cl')[0]
+		self.ispcrcl = np.where([i for i, si in enumerate(self.parlist) if si.startswith('specrecal')])[0]
 		
 		# set some phase/wavelength arrays
 		self.splinecolorwave = np.linspace(self.colorwaverange[0],self.colorwaverange[1],self.n_colorpars)
@@ -445,10 +446,9 @@ class SALTResids:
 				specresultsdict['dmodelflux_dx0'][iSpecStart:iSpecStart+SpecLen,0] = (M0interp + x1*M1interp)
 				specresultsdict['dmodelflux_dx1'][iSpecStart:iSpecStart+SpecLen,0] = x0*M1interp
 				if self.specrecal : 
-					import pdb;pdb.set_trace()
 					specresultsdict['dmodelflux_dspecrecal_{}'.format(k)][iSpecStart:iSpecStart+SpecLen,:] = \
 					modulatedFlux[:,np.newaxis] * \
-					(((specdata[k]['wavelength']-np.mean(specdata[k]['wavelength']))/self.specrange_wavescale_specrecal)[:,np.newaxis] ** np.arange(coeffs.size)[np.newaxis,:]) \
+					(((specdata[k]['wavelength']-np.mean(specdata[k]['wavelength']))/self.specrange_wavescale_specrecal)[:,np.newaxis] ** (coeffs.size-1-np.arange(coeffs.size))[np.newaxis,:]) \
 					/ factorial(np.arange(coeffs.size))[np.newaxis,:]
 
 				