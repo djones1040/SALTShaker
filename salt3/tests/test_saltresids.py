Fixed color law derivative for spectral model uncertainties
Added absolute tolerance to spec residual jacobian test

salt3/tests/test_saltresids.py
@@ -477,6 +477,7 @@ class TRAINING_Test(unittest.TestCase):
 		print("Checking spectral derivatives")
 		dx=1e-8
 		rtol=1e-2
+		atol=1e-4
 		sn=self.resids.datadict.keys().__iter__().__next__()
 		components = self.resids.SALTModel(self.resids.guess)
 		salterr = self.resids.ErrModel(self.resids.guess)
@@ -501,8 +502,8 @@ class TRAINING_Test(unittest.TestCase):
 		for i in range(self.guess.size):
 			dResiddX[:,i]=(incrementOneParam(i,dx)-residuals)/dx
 
-		if not np.allclose(dResiddX,jacobian,rtol): print('Problems with derivatives: ',np.unique(self.parlist[np.where(~np.isclose(dResiddX,jacobian,rtol))[1]]))
-		self.assertTrue(np.allclose(dResiddX,jacobian,rtol))
+		if not np.allclose(dResiddX,jacobian,rtol,atol): print('Problems with derivatives: ',np.unique(self.parlist[np.where(~np.isclose(dResiddX,jacobian,rtol,atol))[1]]))
+		self.assertTrue(np.allclose(dResiddX,jacobian,rtol,atol))
 
 	def test_regularization_jacobian(self):
 		"""Checks that the the jacobian of the spectroscopic residuals is being correctly calculated to within 1%"""

salt3/training/saltresids.py
@@ -532,7 +532,7 @@ class SALTResids:
 			
 				# color law
 				for i in range(self.n_colorpars):
-					dcolorlaw_dcli = SALT2ColorLaw(self.colorwaverange, np.arange(self.n_colorpars)==i)(specdata[k]['wavelength']/(1+z))-SALT2ColorLaw(self.colorwaverange, np.zeros(self.n_colorpars))(specdata[k]['wavelength']/(1+z))
+					dcolorlaw_dcli = interp1d(obswave,SALT2ColorLaw(self.colorwaverange, np.arange(self.n_colorpars)==i)(self.wave)-SALT2ColorLaw(self.colorwaverange, np.zeros(self.n_colorpars))(self.wave),kind=self.interpMethod,bounds_error=False,fill_value=0,assume_sorted=True)(specdata[k]['wavelength'])
 					specresultsdict['modeluncertainty_jacobian'][iSpecStart:iSpecStart+SpecLen,self.iCL[i]] = (-0.4*x0*np.log(10)*c)*modelUncertainty*dcolorlaw_dcli
 			
 				interpresult= self.errorspline_deriv_interp((clippedPhase[0]/(1+z),specdata[k]['wavelength']/(1+z)),method=self.interpMethod) * (intmultnox**2 * x0/2  / modelUncertainty)[:,np.newaxis]