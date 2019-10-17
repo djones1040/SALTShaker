38487ca1d889fda0da33d1f1da3f549b0eb753dc
Justin Roberts-Pierel <jr23@email.sc.edu>
2019-10-17 13:31:14 -0400

    adding binned hubble diagrams

--- salt3/plotting/plots.py
+++ salt3/plotting/plots.py
@@ -1,7 +1,7 @@
 from astropy.cosmology import Planck15 as cosmo
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import os,scipy
 
 
 from .txtobj import txtobj
@@ -20,13 +20,28 @@ def calcMu(fr,alpha=0.14,beta=3.1,M=-19.36):
 	fr.MUERR = np.sqrt(fr.mBERR**2 + alpha**2.*fr.x1ERR**2. + beta**2.*fr.cERR**2)
 	return(fr)
 
-def plot_hubble(fr):
-	ax=plot('errorbar',fr.zCMB,y=fr.MU,yerr=fr.MUERR,y_lab=r'$\mu$',fmt='o')
+def plot_hubble(fr,binned=True):
+	if binned:
+		stats,edges,bins = scipy.stats.binned_statistic(fr.zCMB,fr.MU,'mean')
+		stat_err,edges2,bins2 = scipy.stats.binned_statistic(fr.zCMB,fr.MU,'std',bins=edges)
+		bin_data=[]
+		for i in range(1,len(edges)):
+			inds=np.where(bins==i)
+			bin_data.append(np.average(fr.MU[inds],weights=1./fr.MUERR[inds]))
+		bin_data=np.array(bin_data)
+		ax=plot('errorbar',[(edges[i]+edges[i+1])/2 for i in range(len(edges)-1)],bin_data,yerr=stat_err,y_lab=r'$\mu$',fmt='o')
+		ax,ax2=split_plot(ax,'errorbar',[(edges[i]+edges[i+1])/2 for i in range(len(edges)-1)],
+			y=bin_data-cosmo.distmod([(edges[i]+edges[i+1])/2 for i in range(len(edges)-1)]).value,yerr=stat_err,x_lab=r'$z_{\rm{CMB}}$',y_lab='Residual',fmt='o')
+		lims=ax.get_xlim()
+		ax2.plot(lims,[0,0],'k--',linewidth=3)
+	else:
+		ax=plot('errorbar',fr.zCMB,y=fr.MU,yerr=fr.MUERR,y_lab=r'$\mu$',fmt='o')
+		ax,ax2=split_plot(ax,'errorbar',fr.zCMB,y=fr.MU-cosmo.distmod(fr.zCMB).value,yerr=fr.MUERR,x_lab=r'$z_{\rm{CMB}}$',y_lab='Residual',fmt='o')
+		lims=ax.get_xlim()
+		ax2.plot(lims,[0,0],'k--',linewidth=3)
 	zinterp=np.arange(np.min(fr.zCMB),np.max(fr.zCMB),.01)
 	ax.plot(zinterp,cosmo.distmod(zinterp).value,color='k',linewidth=3)
-	ax,ax2=split_plot(ax,'errorbar',fr.zCMB,y=fr.MU-cosmo.distmod(fr.zCMB).value,yerr=fr.MUERR,x_lab=r'$z_{\rm{CMB}}$',y_lab='Residual',fmt='o')
-	lims=ax.get_xlim()
-	ax2.plot(lims,[0,0],'k--',linewidth=3)
+		
 	if not os.path.exists('figures'):
 		os.makedirs('figures')
 	if os.path.exists(os.path.join('figures',fr.filename+'_hubble_diagram.pdf')):
