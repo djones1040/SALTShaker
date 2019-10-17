64e64e5329bfaf59434c069bdfaf381f82de7083
Justin Roberts-Pierel <jr23@email.sc.edu>
2019-10-17 18:26:57 -0400

    fixing fitting plots

--- salt3/plotting/plots.py
+++ salt3/plotting/plots.py
@@ -79,7 +79,8 @@ def plot_fits(simfile,datafile=None,version=''):
 	ovhist_obj=ovhist()
 	parser = ovhist_obj.add_options(usage=usagestring)
 	options,  args = parser.parse_args()
-	options.histvar = ['x1','c']
+	options.histvar = ['MURES']#['x1','c']
+
 	ovhist_obj.options = options
 	ovhist_obj.version=version
 	if datafile is None:
