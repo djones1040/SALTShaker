Fixed bug: When initializing error model from SALT2, the M0 M1 covariance was initialized from the wrong file
Fixed extremal phase/wavelength values causing zeros in CorrelationModel (same problem as ErrModel bug fix in earlier commit)

Added model uncertainty plots to validation stage

Scaling SALT2 model uncertainties by error snake less 1 in quadrature during initialization from SALT2 and comparison plots

salt3/training/saltresids.py
@@ -1298,7 +1298,9 @@ class SALTResids:
 				binwavecenter =((self.errwaveknotloc)[1:]+(self.errwaveknotloc)[:-1])/2
 				interp=RegularGridInterpolator((binphasecenter,binwavecenter),errpars.reshape(binphasecenter.size,binwavecenter.size),'nearest',False,0)
 				gridwave,gridphase=np.meshgrid(wave,phase)
-				components+=[interp((gridphase.flatten(),gridwave.flatten())).reshape((phase.size,wave.size))]
+				clipinterp=lambda x,y: interp((np.clip(x,binphasecenter.min(),binphasecenter.max()),np.clip(y,binwavecenter.min(),binwavecenter.max())))
+				result=clipinterp(gridphase.flatten(),gridwave.flatten()).reshape((phase.size,wave.size))
+				components+=[result]
 			else:
 				components+=[  bisplev(phase,
 								   wave,
@@ -1316,30 +1318,12 @@ class SALTResids:
 			except: import pdb; pdb.set_trace()
 			if self.errbsorder == 0:
 				binphasecenter=((self.errphaseknotloc)[1:]+(self.errphaseknotloc)[:-1])/2
-				phaseinterp=np.zeros(self.errphaseknotloc.size+1)
-				phaseinterp[1:-1]=binphasecenter
-				phaseinterp[0]=self.errphaseknotloc[0]
-				phaseinterp[-1]=self.errphaseknotloc[-1]
 				binwavecenter =((self.errwaveknotloc)[1:]+(self.errwaveknotloc)[:-1])/2
-				waveinterp=np.zeros(self.errwaveknotloc.size+1)
-				waveinterp[1:-1]=binwavecenter
-				waveinterp[0]=self.errwaveknotloc[0]
-				waveinterp[-1]=self.errwaveknotloc[-1]
 				
-				valinterp=np.zeros((phaseinterp.size,waveinterp.size))
-				valinterp[1:-1,1:-1]=errpars.reshape(binphasecenter.size,binwavecenter.size)
-				valinterp[0,0]=valinterp[1,1]
-				valinterp[-1,-1]=valinterp[-2,-2]
-				valinterp[0,-1]=valinterp[1,-2]
-				valinterp[-1,0]=valinterp[-2,1]
-				valinterp[1:-1,0]=valinterp[1:-1,1]
-				valinterp[1:-1,-1]=valinterp[1:-1,-2]
-				valinterp[0,1:-1]=valinterp[1,1:-1]
-				valinterp[-1,1:-1]=valinterp[-2,1:-1]
-				
-				interp=RegularGridInterpolator((phaseinterp,waveinterp),valinterp,'nearest',False,0)
+				interp=RegularGridInterpolator((binphasecenter,binwavecenter),errpars.reshape(binphasecenter.size,binwavecenter.size),'nearest',False,0)
+				clipinterp=lambda x,y: interp((np.clip(x,binphasecenter.min(),binphasecenter.max()),np.clip(y,binwavecenter.min(),binwavecenter.max())))
 				gridwave,gridphase=np.meshgrid(wave,phase)
-				result=interp((gridphase.flatten(),gridwave.flatten())).reshape((phase.size,wave.size))
+				result=clipinterp(gridphase.flatten(),gridwave.flatten()).reshape((phase.size,wave.size))
 				components+=[result]
 			else:
 				components+=[  bisplev(phase,

salt3/validation/figs/plotSALTModel.py
@@ -10,6 +10,7 @@ from salt3.initfiles import init_rootdir
 from argparse import ArgumentParser
 import logging
 log=logging.getLogger(__name__)
+from scipy.interpolate import bisplrep,bisplev,RegularGridInterpolator
 
 def mkModelPlot(salt3dir='modelfiles/salt3',
 				xlimits=[2000,9200],outfile=None,plotErr=True):
@@ -51,6 +52,15 @@ def mkModelPlot(salt3dir='modelfiles/salt3',
 		np.loadtxt('%s/salt2_lc_relative_variance_0.dat'%init_rootdir,unpack=True)
 	salt2m1errphase,salt2m1errwave,salt2m1fluxerr = \
 		np.loadtxt('%s/salt2_lc_relative_variance_1.dat'%init_rootdir,unpack=True)
+
+	salt2scalephase,salt2scalewave,salt2errscale=np.loadtxt('%s/salt2_lc_dispersion_scaling.dat'%init_rootdir,unpack=True)
+	#Subtract out statistical error from SALT2
+	salt2varscale=(salt2errscale**2-1)
+	salt2scalephase,salt2scalewave=np.unique(salt2scalephase),np.unique(salt2scalewave)
+	scaleinterp=RegularGridInterpolator((salt2scalephase,salt2scalewave),salt2varscale.reshape(salt2scalephase.size,salt2scalewave.size),'nearest')
+	salt2varscaleclipinterp=lambda x,y: scaleinterp((np.clip(x,salt2scalephase.min(),salt2scalephase.max()),np.clip(y,salt2scalewave.min(),salt2scalewave.max())))
+	salt2m0fluxerr*=salt2varscaleclipinterp(salt2m0errphase,salt2m0errwave)
+	salt2m1fluxerr*=salt2varscaleclipinterp(salt2m1errphase,salt2m1errwave)
 
 	salt2m0flux = salt2m0flux.reshape([len(np.unique(salt2m0phase)),len(np.unique(salt2m0wave))])
 	salt2m0fluxerr = salt2m0fluxerr.reshape([len(np.unique(salt2m0errphase)),len(np.unique(salt2m0errwave))])
@@ -199,6 +209,7 @@ def mkModelErrPlot(salt3dir='modelfiles/salt3',outfile=None,xlimits=[2000,9200])
 	ax3 = plt.subplot(413)
 	ax4 = plt.subplot(414)
 
+
 	salt3m0errphase,salt3m0errwave,salt3m0fluxerr = \
 		np.loadtxt('%s/salt3_lc_relative_variance_0.dat'%salt3dir,unpack=True)
 	salt3m1errphase,salt3m1errwave,salt3m1fluxerr = \
@@ -217,14 +228,27 @@ def mkModelErrPlot(salt3dir='modelfiles/salt3',outfile=None,xlimits=[2000,9200])
 	salt2m0m1errphase,salt2m0m1errwave,salt2m0m1fluxerr = \
 		np.loadtxt('%s/salt2_lc_relative_covariance_01.dat'%init_rootdir,unpack=True)
 
+
+
+	salt2scalephase,salt2scalewave,salt2errscale=np.loadtxt('%s/salt2_lc_dispersion_scaling.dat'%init_rootdir,unpack=True)
+	#Subtract out statistical error from SALT2
+	salt2varscale=(salt2errscale**2-1)
+	salt2scalephase,salt2scalewave=np.unique(salt2scalephase),np.unique(salt2scalewave)
+	scaleinterp=RegularGridInterpolator((salt2scalephase,salt2scalewave),salt2varscale.reshape(salt2scalephase.size,salt2scalewave.size),'nearest')
+	salt2varscaleclipinterp=lambda x,y: scaleinterp((np.clip(x,salt2scalephase.min(),salt2scalephase.max()),np.clip(y,salt2scalewave.min(),salt2scalewave.max())))
+	salt2m0fluxerr*=salt2varscaleclipinterp(salt2m0errphase,salt2m0errwave)
+	salt2m1fluxerr*=salt2varscaleclipinterp(salt2m1errphase,salt2m1errwave)
+	salt2m0m1fluxerr*=salt2varscaleclipinterp(salt2m0m1errphase,salt2m0m1errwave)
+	
 	salt2m0fluxerr = salt2m0fluxerr.reshape([len(np.unique(salt2m0errphase)),len(np.unique(salt2m0errwave))])
 	salt2m1fluxerr = salt2m1fluxerr.reshape([len(np.unique(salt2m1errphase)),len(np.unique(salt2m1errwave))])
 	salt2m0m1fluxerr = salt2m0m1fluxerr.reshape([len(np.unique(salt2m0m1errphase)),len(np.unique(salt2m0m1errwave))])
-	
+	salt2m0m1corr=salt2m0m1fluxerr/np.sqrt(salt2m0fluxerr*salt2m1fluxerr)
 	
 	salt3m0fluxerr = salt3m0fluxerr.reshape([len(np.unique(salt3m0errphase)),len(np.unique(salt3m0errwave))])
 	salt3m1fluxerr = salt3m1fluxerr.reshape([len(np.unique(salt3m1errphase)),len(np.unique(salt3m1errwave))])
 	salt3m0m1fluxerr = salt3m0m1fluxerr.reshape([len(np.unique(salt3m0m1errphase)),len(np.unique(salt3m0m1errwave))])
+	salt3m0m1corr=salt3m0m1fluxerr/np.sqrt(salt3m0fluxerr*salt3m1fluxerr)
 	#print('hack!')
 	#salt3m1flux *=-1
 
@@ -236,6 +260,8 @@ def mkModelErrPlot(salt3dir='modelfiles/salt3',outfile=None,xlimits=[2000,9200])
 	salt2m0m1errphase = np.unique(salt2m0m1errphase)
 	salt2m0m1errwave = np.unique(salt2m0m1errwave)
 
+
+	
 	salt3m0errphase = np.unique(salt3m0errphase)
 	salt3m0errwave = np.unique(salt3m0errwave)
 	salt3m1errphase = np.unique(salt3m1errphase)
@@ -245,44 +271,46 @@ def mkModelErrPlot(salt3dir='modelfiles/salt3',outfile=None,xlimits=[2000,9200])
 
 	spacing = 0.5
 	plotwave=np.linspace(2000,9200,720)
-	scale=100
+	scale=2.5
 	for plotphase,i,plotphasestr in zip([-5,0,10],range(3),['-5','+0','+10']):
 		int_salt2m0err = interp2d(salt2m0errwave,salt2m0errphase,salt2m0fluxerr)
-		salt2m0fluxerr_0 = int_salt2m0err(plotwave,plotphase)
+		salt2m0fluxerr_0 = np.sqrt(int_salt2m0err(plotwave,plotphase))
 
 		int_salt3m0err = interp2d(salt3m0errwave,salt3m0errphase,salt3m0fluxerr)
-		salt3m0fluxerr_0 = int_salt3m0err(plotwave,plotphase)
+		salt3m0fluxerr_0 = np.sqrt(int_salt3m0err(plotwave,plotphase))
 
 		ax1.plot(plotwave,salt2m0fluxerr_0*scale+spacing*i,color='b',label='SALT2')
 		ax1.plot(plotwave,salt3m0fluxerr_0*scale+spacing*i,color='r',label='SALT3')
+		ax1.plot(xlimits,[spacing*i,spacing*i],'k--')
 		ax1.set_xlim(xlimits)
 		ax1.set_ylim([0,1.35])
 
 		ax1.text(xlimits[1]-100,spacing*(i+0.2),'%s'%plotphasestr,ha='right')
-		
+	scale=3
 	spacing = 0.15		
 	for plotphase,i,plotphasestr in zip([-5,0,10],range(3),['-5','+0','+10']):
 		int_salt2m1err = interp2d(salt2m1errwave,salt2m1errphase,salt2m1fluxerr)
-		salt2m1fluxerr_0 = int_salt2m1err(plotwave,plotphase)
+		salt2m1fluxerr_0 = np.sqrt(int_salt2m1err(plotwave,plotphase))
 
 		int_salt3m1err = interp2d(salt3m1errwave,salt3m1errphase,salt3m1fluxerr)
-		salt3m1fluxerr_0 = int_salt3m1err(plotwave,plotphase)
+		salt3m1fluxerr_0 = np.sqrt(int_salt3m1err(plotwave,plotphase))
 		ax2.plot(plotwave,salt2m1fluxerr_0*scale+spacing*i,color='b',label='SALT2')
 		ax2.plot(plotwave,salt3m1fluxerr_0*scale+spacing*i,color='r',label='SALT3')
+		ax2.plot(xlimits,[spacing*i,spacing*i],'k--')
 		ax2.set_xlim(xlimits)
 		ax2.set_ylim([-0.05,0.39])
 
 		ax2.text(xlimits[1]-100,spacing*(i+0.2),'%s'%plotphasestr,ha='right')
-	scale=100
-
+	scale=.2
 	for plotphase,i,plotphasestr in zip([-5,0,10],range(3),['-5','+0','+10']):
-		int_salt2m0m1err = interp2d(salt2m0m1errwave,salt2m0m1errphase,salt2m0m1fluxerr)
-		salt2m0m1fluxerr_0 = int_salt2m0m1err(plotwave,plotphase)
+		int_salt2m0m1corr = interp2d(salt2m0m1errwave,salt2m0m1errphase,salt2m0m1corr)
+		salt2m0m1fluxerr_0 = int_salt2m0m1corr(plotwave,plotphase)
 
-		int_salt3m0m1err = interp2d(salt3m0m1errwave,salt3m0m1errphase,salt3m0m1fluxerr)
-		salt3m0m1fluxerr_0 = int_salt3m0m1err(plotwave,plotphase)
+		int_salt3m0m1corr = interp2d(salt3m0m1errwave,salt3m0m1errphase,salt3m0m1corr)
+		salt3m0m1fluxerr_0 = int_salt3m0m1corr(plotwave,plotphase)
 		ax3.plot(plotwave,salt2m0m1fluxerr_0*scale+spacing*i,color='b',label='SALT2')
 		ax3.plot(plotwave,salt3m0m1fluxerr_0*scale+spacing*i,color='r',label='SALT3')
+		ax3.plot(xlimits,[spacing*i,spacing*i],'k--')
 		ax3.set_xlim(xlimits)
 		ax3.set_ylim([-0.05,0.39])
 
@@ -300,7 +328,7 @@ def mkModelErrPlot(salt3dir='modelfiles/salt3',outfile=None,xlimits=[2000,9200])
 	ax4.legend(prop={'size':13})
 	ax1.set_ylabel('M0 dispersion',fontsize=15)
 	ax2.set_ylabel('M1 dispersion',fontsize=15)
-	ax3.set_ylabel('M0-M1 covariance',fontsize=15)
+	ax3.set_ylabel('M0-M1 correlation',fontsize=15)
 	ax4.set_ylabel('Color Scatter',fontsize=15)
 	for ax in [ax1,ax2,ax3,ax4]:
 		ax.set_xlim(xlimits)