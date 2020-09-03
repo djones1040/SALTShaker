2bc7ce7daabdbd73e21cb329f7a961893c84d283
djones1040 <djones@pha.jhu.edu>
2020-09-03 12:46:23 -0700

    python scripts

---
+++ examples/JLA_training_orig_sim/mypipe.py
@@ -0,0 +1,11 @@
+from salt3.pipeline.pipeline import SALT3pipe
+
+def MyPipe(finput,**kwargs):
+    pipe = SALT3pipe(finput=finput)
+    pipe.build(data=False,mode='customize',
+			   onlyrun=['train','lcfit'])
+    pipe.configure()
+    pipe.glue(['train','lcfit'],on='model')
+
+    return pipe
+
