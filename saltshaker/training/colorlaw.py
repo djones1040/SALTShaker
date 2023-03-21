8d75d5ca14391710aae6d71ef53b1b80baa0e458
David Jones <david.jones@ucsc.edu>
2023-03-21 09:35:32 -1000

    SALT2 colorlaw function in jax

--- saltshaker/training/colorlaw.py
+++ saltshaker/training/colorlaw.py
@@ -1,12 +1,55 @@
 import numpy as np
 from sncosmo.salt2utils import SALT2ColorLaw
 from scipy.interpolate import interp1d
+from jax import numpy as jnp
+import jax
 
 __colorlaws__=dict()
 def colorlaw(colorlaw):
     """Decorator to register a given function as a valid prior"""
     __colorlaws__[colorlaw.__name__]=colorlaw
     return colorlaw
+
+SALT2CL_B = 4302.57
+SALT2CL_V = 5428.55
+SALT2CL_V_MINUS_B = SALT2CL_V - SALT2CL_B
+
+class SALT2ColorLaw:
+    def __init__(self,wave_range,coeffs):
+        
+        if len(coeffs) > 6:
+            raise ValueError("number of coefficients must be equal to or "
+                             "less than 6.")
+
+        wave_lo, wave_hi = wave_range
+        self.l_lo = (wave_lo - SALT2CL_B) / SALT2CL_V_MINUS_B
+        self.l_hi = (wave_hi - SALT2CL_B) / SALT2CL_V_MINUS_B
+
+        self.coeffs = jnp.concatenate((coeffs[::-1],jnp.array([1.0-jnp.sum(coeffs)]),jnp.array([0.])))
+        self.ncoeffs = len(self.coeffs)
+
+        # precompute value of
+        # P(l) = c[0]*l + c[1]*l^2 + c[2]*l^3 + ...  at l_lo and l_hi
+        self.p_lo = jnp.polyval(self.coeffs, self.l_lo)
+        self.p_hi = jnp.polyval(self.coeffs, self.l_hi)
+
+        # precompute derivative of P(l) at l_lo and l_hi
+        fun = lambda x: jax.grad(jnp.polyval,argnums=1)(self.coeffs,x)
+        self.pprime_lo = fun(self.l_lo)
+        self.pprime_hi = fun(self.l_hi)
+        
+    def __call__(self,wave):
+
+        #for i in range(n):
+        l = (wave - SALT2CL_B) / SALT2CL_V_MINUS_B
+
+        # Blue side
+        return -1* jnp.select(
+            [l < self.l_lo,l <= self.l_hi,l > self.l_hi],
+            [self.p_lo + self.pprime_lo * (l - self.l_lo),
+             jnp.polyval(self.coeffs, l),
+             self.p_hi + self.pprime_hi * (l - self.l_hi)])
+
 
 @colorlaw
 class colorlaw_default:
