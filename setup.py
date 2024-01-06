7085d37c2b5cc2d0482601a66057b8d06b060fe9
David Jones <david.jones@ucsc.edu>
2024-01-05 14:44:25 -1000

    new version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class SALTShakerTest(TestCommand):
 
 AUTHOR = 'David Jones, Rick Kessler'
 AUTHOR_EMAIL = 'david.jones@ucsc.edu'
-VERSION = '1.2'
+VERSION = '1.3'
 LICENSE = 'BSD'
 URL = 'saltshaker.readthedocs.org'
 
