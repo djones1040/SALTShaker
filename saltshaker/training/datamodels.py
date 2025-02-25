Temporary fix adding a stop gradient to the spectral model to prevent model separating out spectra by x1^2 Fix to chi2 printout bug using untransformed values

saltshaker/training/datamodels.py
@@ -548,7 +548,7 @@ class modeledtrainingspectrum(modeledtrainingdata):
     
         variance=self.fluxerr**2 + modelvariance
       
-        uncertainty=jnp.sqrt(variance)
+        uncertainty=jax.lax.stop_gradient(jnp.sqrt(variance))
         
         numresids=(~self.ipad).sum() 
         zeropoint= ( -jnp.log(self.fluxerr).sum() - numresids/2)