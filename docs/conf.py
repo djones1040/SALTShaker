080f54586c86bd356ff7555b98e57a9ba1871f56
darcykenworthy <darcykenworthy@me.com>
2026-01-14 12:32:32 -0600

    Fixed Sphinx documentation warnings and errors

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ release = ''
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -123,7 +123,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path is no longer needed with modern sphinx_rtd_theme
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
